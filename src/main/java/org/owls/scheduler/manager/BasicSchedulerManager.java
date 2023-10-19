3fd91434de043467efea1b3de724ec0e8f8be53c
jyoh <jyoh@hancomwith.com>
2023-10-19 20:32:10 +0900

    add finalize method to manager

--- src/main/java/org/owls/scheduler/manager/BasicSchedulerManager.java
+++ src/main/java/org/owls/scheduler/manager/BasicSchedulerManager.java
@@ -82,4 +82,10 @@ public class BasicSchedulerManager implements ScheduleManager {
     public ManagerStatus getStatus() {
         return this.status;
     }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        this.stop();
+    }
 }
